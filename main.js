0501006de2afd06996f4d7080698afaaa8349aee
Simon Zhang <simonzhang96@icloud.com>
2026-06-03 21:59:36 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 import * as THREE from "https://unpkg.com/three@0.179.1/build/three.module.js";
 import { OrbitControls } from "https://unpkg.com/three@0.179.1/examples/jsm/controls/OrbitControls.js";
 
+console.log("three loaded OK");
+
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  60,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
 camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -13,7 +21,7 @@ document.body.appendChild(renderer.domElement);
 scene.add(new THREE.AmbientLight(0xffffff, 2));
 
 const loader = new THREE.TextureLoader();
-const texture = loader.load("./Zeksa-Omklon-Map.png");
+const texture = loader.load("./textures/omklon_map.png");
 
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(1, 64, 64),
@@ -31,5 +39,3 @@ function animate() {
 }
 
 animate();
-
-
