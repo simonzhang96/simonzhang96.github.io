73b49bda7d88a4a2ca9a9ee35f52ae42bee2d445
Simon Zhang <simonzhang96@icloud.com>
2026-06-04 19:47:55 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from "https://unpkg.com/three@0.179.1/build/three.module.js";
-import { OrbitControls } from "https://unpkg.com/three@0.179.1/examples/jsm/controls/OrbitControls.js";
+import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
+import { OrbitControls } from "https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js";
 
 console.log("three loaded OK");
 
